109a526d4428ffa954d1f4402363a23c58a0ab4a
TanFuc <nguyentannphuc@gmail.com>
2024-08-27 23:13:09 +0700

    four

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button";
+"use client"
+import { useEffect } from "react";
+import { useStoreModal } from "@/hooks/use-store-modal";
 
-export default function Home() {
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen)
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
   return (
     <div className="p-4">
-      <Button>Click me</Button>
+       Root Page
     </div>
   );
 }
+
+export default SetupPage;
